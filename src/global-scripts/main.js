a1c406b2e912f60168c3daf664b13152e09f7c3d
Jan Hickiewicz <janhi@tutanota.com>
2023-03-09 19:26:21 +0100

    add script for changing button state

--- src/global-scripts/main.js
+++ src/global-scripts/main.js
@@ -1,5 +1,15 @@
 /// Main JavaScript File
 /// Here we import all the global JavaScript files we need for our project.
 
-import '../global-styles/style.scss'
+import "../global-styles/style.scss";
 
+{
+  const proceedButton = document.querySelector(".proceedButton-js");
+
+  proceedButton.addEventListener("click", () => {
+    proceedButton.textContent = "";
+    const loaderDiv = document.createElement("div");
+    loaderDiv.classList.add("loader");
+    proceedButton.appendChild(loaderDiv);
+  });
+}
